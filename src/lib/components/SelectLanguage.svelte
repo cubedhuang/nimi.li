5dc2451d9a71faccffeee5437d2be342b631be8a
cubedhuang <99danielh@gmail.com>
2025-09-05 14:50:58 -0400

    improve spacing and search border

--- src/lib/components/SelectLanguage.svelte
+++ src/lib/components/SelectLanguage.svelte
@@ -82,91 +82,88 @@
 		</Popover.Trigger>
 
 		<Popover.Portal disabled>
-			<Popover.ContentStatic forceMount child={command}
-			></Popover.ContentStatic>
+			<Popover.ContentStatic forceMount>
+				{#snippet child({ props, open })}
+					{#if open}
+						<div
+							{...props}
+							transition:flyAndScale={{ y: -8 }}
+							class="absolute top-full left-0 z-50 mt-1 h-80 w-full rounded-md border-2 bg-card text-card-foreground shadow-md"
+						>
+							{@render command()}
+						</div>
+					{/if}
+				{/snippet}
+			</Popover.ContentStatic>
 		</Popover.Portal>
 	</Popover.Root>
 </div>
 
-{#snippet command({
-	props,
-	open
-}: {
-	props: Record<string, unknown>;
-	open: boolean;
-})}
-	{#if open}
-		<div
-			{...props}
-			transition:flyAndScale={{ y: -8 }}
-			class="absolute top-full left-0 z-50 mt-1 h-80 w-full rounded-md border-2 bg-card text-card-foreground shadow-md"
-		>
-			<Command.Root
-				shouldFilter={false}
-				class="flex h-full flex-col overflow-hidden"
+{#snippet command()}
+	<Command.Root
+		shouldFilter={false}
+		class="flex h-full flex-col overflow-hidden"
+	>
+		<div class="shrink-0 border-b-2 p-1">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 16 16"
+				fill="currentColor"
+				class="absolute top-2.5 left-2 size-4"
 			>
-				<div class="shrink-0 border-b-2 bg-card p-1">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						fill="currentColor"
-						class="absolute top-2.5 left-2 size-4"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					<Command.Input
-						placeholder="o alasa..."
-						class="w-full appearance-none rounded-md px-2 py-0.5 pl-6 outline-hidden placeholder:text-muted"
-						bind:value={search}
-					/>
-				</div>
-
-				<Command.List class="flex-1 overflow-y-auto">
-					<Command.Empty class="py-0.5 pl-7 text-muted">
-						No language found.
-					</Command.Empty>
-
-					<Command.Group class="flex flex-col gap-1 p-1">
-						{#each shownLanguages as option (option.id)}
-							<Command.Item
-								class="group w-full cursor-pointer select-none"
-								value={option.id}
-								onSelect={() => {
-									onchange(option.id);
-									closeAndFocusTrigger();
-								}}
-							>
-								{@const isSelected = $language === option.id}
-								<span
-									class="relative flex items-center rounded-md px-2 py-0.5 pl-6
-										group-data-selected:bg-background group-data-selected:text-accent light:group-data-selected:bg-secondary darkish:group-data-selected:bg-secondary"
-								>
-									{#if isSelected}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 16 16"
-											fill="currentColor"
-											class="absolute left-1 size-4"
-										>
-											<path
-												fill-rule="evenodd"
-												d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-									{/if}
-
-									{option.name.endonym ?? option.name.en}
-								</span>
-							</Command.Item>
-						{/each}
-					</Command.Group>
-				</Command.List>
-			</Command.Root>
+				<path
+					fill-rule="evenodd"
+					d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+			<Command.Input
+				placeholder="o alasa..."
+				class="w-full appearance-none rounded-md px-2 py-0.5 pl-6 outline-hidden placeholder:text-muted"
+				bind:value={search}
+			/>
 		</div>
-	{/if}
+
+		<Command.List class="flex-1 overflow-y-auto">
+			<Command.Empty class="py-1.5 pl-7 text-muted">
+				No language found.
+			</Command.Empty>
+
+			<Command.Group class="flex flex-col gap-1 p-1">
+				{#each shownLanguages as option (option.id)}
+					<Command.Item
+						class="group w-full cursor-pointer select-none"
+						value={option.id}
+						onSelect={() => {
+							onchange(option.id);
+							closeAndFocusTrigger();
+						}}
+					>
+						{@const isSelected = $language === option.id}
+						<span
+							class="relative flex items-center rounded-md px-2 py-0.5 pl-6
+										group-data-selected:bg-background group-data-selected:text-accent light:group-data-selected:bg-secondary darkish:group-data-selected:bg-secondary"
+						>
+							{#if isSelected}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 16 16"
+									fill="currentColor"
+									class="absolute left-1 size-4"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+							{/if}
+
+							{option.name.endonym ?? option.name.en}
+						</span>
+					</Command.Item>
+				{/each}
+			</Command.Group>
+		</Command.List>
+	</Command.Root>
 {/snippet}
